063070a585a910561d41465cd0e06ba7ef3db0db
cabraljv <joaovictorcabral.dev@gmail.com>
2020-08-20 20:45:58 -0300

    feat: add save pdf

---
+++ src/savePDF.js
@@ -0,0 +1,26 @@
+const pdfmake = require('pdfmake');
+const fs = require('fs')
+const fonts = {
+  Roboto: {
+    normal: './assets/fonts/Roboto-Regular.ttf',
+    bold: './assets/fonts/Roboto-Bold.ttf',
+    italics: './assets/fonts/Roboto-Italic.ttf',
+    bolditalics: './assets/fonts/Roboto-BoldItalic.ttf'
+  },
+}
+var printer = new pdfmake(fonts);
+function gerarPdf(texto){
+  var docDefinition = {
+    content: [
+      texto
+    ]
+  };
+  
+  var now = new Date();
+  
+  var pdfDoc = printer.createPdfKitDocument(docDefinition);
+  pdfDoc.pipe(fs.createWriteStream(`${now.valueOf()}-respostas.pdf`));
+  pdfDoc.end();
+}
+module.exports = gerarPdf;
+
